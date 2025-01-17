46667ef3b878c9e786e78fe6346be6b4076756a5
Ajay M <ajaym@indiaadvocacy.com>
2025-01-17 14:52:22 +0530

    Update Majority(nby2).java
    adding majority n/2

--- ARRAYS/Majority(nby2).java
+++ ARRAYS/Majority(nby2).java
@@ -1,1 +1,37 @@
-
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        
+        int element=0;
+        for(int i=0;i<n;i++)
+        {
+             if(count==0)
+             {
+                element=nums[i];
+                count=1;
+             }
+             else if(nums[i]==element)
+             {
+                count++;
+             }
+             else
+             {
+                count--;
+             }
+        }
+        count=0;
+        for(int e:nums)
+        {
+            if(e==element)
+            {
+                count++;
+            }
+        }
+        if(count>n/2)
+        {
+            return element;
+        }
+    return 0;
+}
+}
