0b59b874ae5ede1b93140eb8650ba10d2bdd1513
Ajay M <ajaym@indiaadvocacy.com>
2025-01-22 18:47:59 +0530

    Create SinglyLinkedListTraversal.java

---
+++ LinkedList/Basics/SinglyLinkedListTraversal.java
@@ -0,0 +1,42 @@
+package com.dsa.linkedlist;
+class Node{
+	int data;
+	Node next;
+	Node(int data,Node next)
+	{
+		this.data = data;
+		this.next = next;
+	}
+	Node(int data)
+	{
+		this.data = data;
+		this.next = null;
+	}
+}
+
+public class ArrayToLinkedList {
+	static Node createLinkedList(int[] nums)
+	{
+	      Node head = new Node(nums[0]);
+	      Node mover = head;
+	      for(int i=1;i<nums.length;i++)
+	      {
+	    	  Node temp = new Node(nums[i]);
+	    	  mover.next = temp;
+	    	  mover = temp;
+	      }
+	      return head;
+	}
+    public static void main(String[] args) {
+		int[] arr = {100,200,300,400,500,600};
+		Node head = createLinkedList(arr);
+		Node temp = head;
+		System.out.println("**********");
+		while(temp != null)
+		{
+		    System.out.println(temp.data);
+		    temp = temp.next;
+		}
+		System.out.println("**********");
+	}
+}
