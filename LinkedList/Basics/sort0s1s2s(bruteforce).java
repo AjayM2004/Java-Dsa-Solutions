4c8e6f327e53ef7da164d9318f2b9b0c78612d2b
Ajay M <ajaym@indiaadvocacy.com>
2025-03-05 08:14:59 +0530

    Update sort0s1s2s(bruteforce).java

--- LinkedList/Basics/sort0s1s2s(bruteforce).java
+++ LinkedList/Basics/sort0s1s2s(bruteforce).java
@@ -1,1 +1,43 @@
 
+class Solution {
+    // Function to sort a linked list of 0s, 1s and 2s.
+    static Node segregate(Node head) 
+    {
+        int count0=0;
+        int count1=0;
+        int count2=0;
+        Node temp = head;
+        while(temp!=null)
+        {
+            if(temp.data==0)
+            {
+                count0++;
+            }
+            else if(temp.data==1)
+            {
+                count1++;
+            }
+            else{
+                count2++;
+            }
+            temp = temp.next;
+        }
+        temp = head;
+        for(int i=1;i<=count0;i++)
+        {
+            temp.data = 0;
+            temp = temp.next;
+        }
+        for(int i=1;i<=count1;i++)
+        {
+            temp.data = 1;
+            temp = temp.next;
+        }
+        for(int i=1;i<=count2;i++)
+        {
+            temp.data = 2;
+            temp = temp.next;
+        }
+        return head;
+    }
+}
