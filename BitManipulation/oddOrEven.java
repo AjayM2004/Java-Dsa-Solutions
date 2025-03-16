81c71a904a08d60ce1e0969b8049b6e81085df50
Ajay M <ajaym@indiaadvocacy.com>
2025-03-16 19:05:42 +0530

    Create oddOrEven.java

---
+++ BitManipulation/oddOrEven.java
@@ -0,0 +1,12 @@
+class Solution {
+    static boolean isEven(int n) {
+      int ans = n&1;
+      if(ans==0)
+      {
+          return true;
+      }
+      else{
+          return false;
+      }
+    }
+}
